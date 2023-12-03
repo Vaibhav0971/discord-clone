53ed4cb1eed0f27b63f2ad7b7bb308aa88e83853
Vaibhav0971 <52752320+Vaibhav0971@users.noreply.github.com>
2023-12-03 19:23:24 +0530

    Added creds for demo user login

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,16 @@
 import { ReactNode } from 'react';
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <div className='h-full flex items-center justify-center'>{children}</div>;
+  return (
+    <div className='h-full flex flex-col items-center justify-center'>
+      <h1 className='flex text-4xl mb-2'>Discord Clone</h1>
+      <p className='flex mb-8'>Made by - Vaibhav Sharma </p>
+      {children}
+      <p className='flex text-lg mt-8'>Demo User Credentials</p>
+      <p className='flex text-sm mt-2'>Email - temp@yopmail.com</p>
+      <p className='flex text-sm'>Password - temp@yopmail.com</p>
+    </div>
+  );
 };
 
 export default AuthLayout;
